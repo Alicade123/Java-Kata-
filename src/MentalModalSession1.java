8ef0e7b14931cdbd2bcaf5d8787aca15ec3cefe3
Alicade Abiturije Dusabe <abiturije1alicade@gmail.com>
2025-11-03 20:03:07 +0200

    The Java learingi progress about the 1st Mental medal session conducted

--- src/MentalModalSession1.java
+++ src/MentalModalSession1.java
@@ -1,4 +1,29 @@
-package PACKAGE_NAME;
-
 public class MentalModalSession1 {
+    public static void main(){
+//        String name1 = "Alicade";
+//        String name2 = "Alicade";
+//        System.out.println(name1.equals(name2));//true
+//        System.out.println(name1==name2);//true
+//
+//        String companyName1 = new String("The Gym");
+//        String companyName2 = new String("The Gym");
+//        System.out.println(companyName1.equals(companyName2));//true
+//        System.out.println(companyName1==companyName2); //false
+//
+//        int x = 5;
+//        int y = x;
+//        x = 10;
+//        System.out.println(x); //10
+//        System.out.println(y);//5
+//        System.out.println(x==y);//false
+
+        String a = new String("Hello");
+        String b = a;
+        a = "World";
+        System.out.println(a); // World
+        System.out.println(b); //Hello
+
+
+    }
+
 }
