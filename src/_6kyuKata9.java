01000679b0a7d7b92e152c6c570c4e47dc9418de
Alicade Abiturije Dusabe <abiturije1alicade@gmail.com>
2025-11-03 20:01:28 +0200

    The new kata challenge : 6kyu

--- src/_6kyuKata9.java
+++ src/_6kyuKata9.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
-
 public class _6kyuKata9 {
+    public static void main(String[] args){
+        System.out.println(solve(")()("));//2
+        System.out.println(solve("((()"));//1
+        System.out.println(solve("((("));//-1
+        System.out.println(solve("())((("));//3
+        System.out.println(solve("())()))))()()("));//4
+    }
+    public static int solve(String s){
+        if(s.length()%2!=0) return -1;
+        int balance = 0;
+        int unmatch = 0;
+       for(int i=0;i<s.length();i++){
+           if(s.charAt(i)=='('){
+               balance++;
+           }else if(s.charAt(i)==')' &&  balance>0){
+               balance--;
+           }else {
+               unmatch++;
+           }
+       }
+       return balance+unmatch;
+    }
 }
