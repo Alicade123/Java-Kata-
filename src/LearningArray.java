4aff1feb0591353e5bdc7219a812f9d4d4a4f5dc
Alicade Abiturije Dusabe <abiturije1alicade@gmail.com>
2025-11-03 20:02:06 +0200

    The New learning progress of java about Arrays

--- src/LearningArray.java
+++ src/LearningArray.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
-
-public class LearningArray {
-}
+//
+//import java.lang.reflect.Array;
+//import java.util.Scanner;
+//
+//public class LearningArray {
+//
+//    public static void main(String[] args) {
+//Scanner scanner = new Scanner(System.in);
+//        System.out.println("Enter the number of elements in the array: ");
+//        int n = scanner.nextInt();
+//        for (int i = 0; i < n; i++) {
+//            System.out.println("Enter the value of " + i + ": ");
+//            int element = scanner.nextInt();
+//            Array.toS
+//
+//        }
+//scanner.close();
+//    }
+//}
