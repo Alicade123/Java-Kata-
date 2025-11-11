2869743409aa15621014387c4a4a5e061bc7f21b
Alicade Abiturije Dusabe <abiturije1alicade@gmail.com>
2025-11-11 22:25:08 +0200

    The new Kata Challenge which is about to sum up all positive number found onto array

--- src/_8kyu_SumOfPositive.java
+++ src/_8kyu_SumOfPositive.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
-
 public class _8kyu_SumOfPositive {
+    public static void main(String[] args) {
+        System.out.println(sum(new int[]{1,2,3,4,5}));
+        System.out.println(sum(new int[]{1,-2,3,4,5}));
+        System.out.println(sum(new int[]{}));
+        System.out.println(sum(new int[]{-1,-2,-3,-4,-5}));
+        System.out.println(sum(new int[]{-1,2,3,4,-5}));
+    }
+    public static int sum(int[] arr){
+        int sum = 0;
+       for(int num : arr){
+           if(num > 0){
+               sum += num;
+           }
+       }
+        return sum;
+    }
+
 }
