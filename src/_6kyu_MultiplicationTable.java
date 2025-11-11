269a788826a608f49b61b223c18a98e2f0c66deb
Alicade Abiturije Dusabe <abiturije1alicade@gmail.com>
2025-11-11 22:22:43 +0200

    The new Kata Challenge that will implement the method that accept one prameter and return the multiplication table grid

--- src/_6kyu_MultiplicationTable.java
+++ src/_6kyu_MultiplicationTable.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
+import java.util.Arrays;
 
 public class _6kyu_MultiplicationTable {
+    public static void main(String[] args) {
+System.out.println(Arrays.deepToString(multiplicationTable(1)));
+System.out.println(Arrays.deepToString(multiplicationTable(3)));
+    }
+    public static int [][] multiplicationTable(int n){
+
+       int [][] results = new int [n][n];
+       for(int i=1;i<=n;i++){
+           for(int j=1;j<=n;j++){
+               results[i-1][j-1]=i*j;
+           }
+       }
+       return results;
+    }
 }
