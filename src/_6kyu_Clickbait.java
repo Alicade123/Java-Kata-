900915e981387ab8f9b692ef43e8e3e009d7397e
Alicade Abiturije Dusabe <abiturije1alicade@gmail.com>
2025-11-28 20:05:25 +0200

    The small change i made on the kata but it's uncompleted

--- src/_6kyu_Clickbait.java
+++ src/_6kyu_Clickbait.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
+import java.util.Arrays;
 
 public class _6kyu_Clickbait {
+    public static void main(String[] args) {
+        System.out.println(Arrays.deepToString(shift(new char[][]{{'a','b','c','d'},
+                {'1','2','3','4'},
+                {'c','o','d','e'},
+                {'b','l','a','h'}}, 1)));
+    }
+    public static char[][] shift(char[][]m, int n){
+        char [][] result = new char[m.length][m[0].length];
+        int row = n%m.length;
+        for(int i=0;i<m.length;i++){
+            for(int j=0;j<m[0].length;j++){
+
+
+            }
+        }
+        return result;
+
+    }
 }
