7c7b93a677cc320466b475bbb6d4d1834a680e93
Alicade Abiturije Dusabe <abiturije1alicade@gmail.com>
2025-11-30 18:19:12 +0200

    Your job is to create a calculator which evaluates expressions in Reverse Polish notation.

---
+++ src/_6kyu_ReversePolishNotationCalculator.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class _6kyu_ReversePolishNotationCalculator {
+    public static void main(String[] args){
+        System.out.println(evaluate("5 1 2 + 4 * + 3 -"));
+        System.out.println(evaluate("5 1 2 + 4 * - 3 -"));
+        System.out.println(evaluate(""));
+        System.out.println(evaluate("1 3 +"));
+        System.out.println(evaluate("3.5"));
+    }
+    public static double evaluate(String expr) {
+        // TODO: Your awesome code here
+        if(expr.isEmpty()) return  0;
+        List<String> stack = new ArrayList<>();
+        int result=0;
+        for(String digit : expr.split(" ")){
+            if (digit.equals("/")||digit.equals("*")||digit.equals("-")||digit.equals("+")){
+            int first = Integer.parseInt(stack.get(stack.size()-2));
+            int second = Integer.parseInt(stack.getLast());
+            stack.remove(stack.size()-2);
+            stack.removeLast();
+            switch (digit){
+                case "/" -> stack.add(first / second+"");
+                case "-" -> stack.add(first - second+"");
+                case "*" -> stack.add(first * second+"");
+                case "+" -> stack.add(first + second+"");
+            }
+            }else{
+                stack.add(digit);
+            }
+        }
+        return  Double.parseDouble(stack.getFirst());
+    }
+}
