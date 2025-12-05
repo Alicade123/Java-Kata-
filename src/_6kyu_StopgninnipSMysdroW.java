3a4beea6ea087cb20063b0546cb9f1df61da6f10
Alicade Abiturije Dusabe <abiturije1alicade@gmail.com>
2025-12-05 11:04:52 +0200

    the new kata challenge: the task here is Write a function that takes in a string of one or more words, and returns the same string, but with all words that have five or more letters reversed (just like the name of this kata). Strings passed in will consist of only letters and spaces. Spaces will be included only when more than one word is present.

--- src/_6kyu_StopgninnipSMysdroW.java
+++ src/_6kyu_StopgninnipSMysdroW.java
@@ -1,4 +1,28 @@
-package PACKAGE_NAME;
-
 public class _6kyu_StopgninnipSMysdroW {
+    public  void main(String[] args){
+        System.out.println(spinWords("This is a test")); //This is a test"
+        System.out.println(spinWords("This is another test")); //This is rehtona test"
+        System.out.println(spinWords("Welcome")); // emocleW
+        System.out.println(spinWords("Hey fellow Warriors")); //Hey wollef sroirraw
+    }
+
+    public String spinWords(String sentence) {
+        //TODO: Code stuff here
+        StringBuilder result = new StringBuilder();
+        String[] words  = sentence.split(" ");
+        int counter = 0;
+        for(String word : words){
+            counter++;
+            if(word.length()>4) {
+                if(counter == words.length ) result.append(new StringBuilder(word).reverse());
+                else result.append(new StringBuilder(word).reverse()).append(" ");
+            }
+
+            else {
+                if(counter == words.length ) result.append(word);
+                else result.append(word).append(" ");
+            }
+        }
+        return result.toString();
+    }
 }
