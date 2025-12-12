e37aa1acc8b36b33385106010f1d33fd977f2402
Alicade Abiturije Dusabe <abiturije1alicade@gmail.com>
2025-12-12 20:55:37 +0200

    The small changes i made to the persistendBugger kata challenge to make it more efficient readable and reduce the unnecessary lines

--- src/_6kyu_PersistentBugger.java
+++ src/_6kyu_PersistentBugger.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
-
 public class _6kyu_PersistentBugger {
+    public static void main (){
+        System.out.println(persistence( 39));//3
+        System.out.println(persistence( 4));//0
+        System.out.println(persistence( 25));//2
+        System.out.println(persistence( 999)); //4
+
+    }
+    public static int persistence(long n) {
+    int count =0;
+    String myNumber = String.valueOf(n);
+    if(myNumber.length()==1) return count;
+    else{
+       while (myNumber.length()>1){
+           count++;
+           int multiplication = 1;
+           for(int i =0; i< myNumber.length();i++) multiplication *= Integer.parseInt(myNumber.charAt(i)+"");
+           myNumber = String.valueOf(multiplication);
+       }
+    }
+        return count ;
+    }
 }
