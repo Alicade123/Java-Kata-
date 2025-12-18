f4484e3cf07a4bb3a1519250a7ef9a4e987d283e
Alicade Abiturije Dusabe <abiturije1alicade@gmail.com>
2025-12-18 12:36:17 +0200

    Just like in the 'father' kata, you will have to return the last digit of the nth element in the Fibonacci sequence (starting with 1,1, to be extra clear, not with 0,1 or other numbers). Notice: You will just get much bigger numbers, so good luck bruteforcing your way through it ;)

--- src/_6kyu_FindLastFibonacciDigit_HardcoreVersion.java
+++ src/_6kyu_FindLastFibonacciDigit_HardcoreVersion.java
@@ -1,4 +1,30 @@
-package PACKAGE_NAME;
+import java.util.Arrays;
 
 public class _6kyu_FindLastFibonacciDigit_HardcoreVersion {
+    public static void main(String[] args){
+        System.out.println(testLast(1));
+        System.out.println(testLast(2));
+        System.out.println(testLast(3));
+        System.out.println(testLast(4));
+        System.out.println(testLast(5));
+        System.out.println(testLast(10));
+        System.out.println(testLast(100));
+        System.out.println(testLast(1000));
+        System.out.println(testLast(1000000));
+    }
+    public  static int  testLast(int number){
+        if(number>2){
+            int first = 1;
+            int second = 1;
+            number = number%60;
+            if (number == 0) number = 60;
+                for (int i=3; i<=number;i++){
+                    int next = (first+second)%10;
+                    first =second;
+                    second =next;
+                }
+            return  second;
+        }
+        else return 1;
+    }
 }
