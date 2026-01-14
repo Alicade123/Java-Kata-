646d0fc0fac2adf4a4e844f173b95b7f36ce7846
Alicade Abiturije Dusabe <abiturije1alicade@gmail.com>
2026-01-14 09:21:50 +0200

    The new kata challenge 6kyu - Write a function that takes a string and turns any and all 'words' within that string of length 4 or greater into an abbreviation

--- src/_6kyu_Abbreviator.java
+++ src/_6kyu_Abbreviator.java
@@ -1,4 +1,36 @@
-package PACKAGE_NAME;
+import java.util.Arrays;
+import java.util.regex.Pattern;
 
 public class _6kyu_Abbreviator {
+    static void main() {
+        System.out.println(abbreviate("internationalization"));//i18n
+        System.out.println(abbreviate("are"));//are
+        System.out.println(abbreviate("rides"));//r3s
+        System.out.println(abbreviate("elephant-rides are really fun!"));//e6t-r3s are r4y fun!
+    }
+    public static String abbreviate(String string) {
+        StringBuilder result = new StringBuilder();
+        StringBuilder word = new StringBuilder();
+     for (char c:string.toCharArray()){
+         if(Character.isLetter(c)) word.append(c);
+         else{
+             transform(result, word);
+             result.append(c);
+             word.setLength(0);
+         }
+     }
+        transform(result, word);
+     return result.toString();
+    }
+    public static void transform(StringBuilder res, StringBuilder word){
+        if(word.length()>=4){
+            res.append(word.charAt(0));
+            int counter = 0;
+            for(int i=1;i<word.length()-1;i++){
+                counter++;
+            }
+            res.append(counter).append(word.charAt(word.length()-1));
+        }
+        else res.append(word);
+    }
 }
