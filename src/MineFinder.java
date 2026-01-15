86e25131681607aa52bb836e64da168e20fa6f74
Alicade Abiturije Dusabe <abiturije1alicade@gmail.com>
2026-01-15 09:45:24 +0200

    The new kata challenge 6kyu - To find the cordinates the mine is placed in the yard

---
+++ src/MineFinder.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+public class MineFinder {
+    public static void main(String[] args){
+        System.out.println(Arrays.toString(findMine( new int[][] { {1, 0}, {0, 0} } )));//new int[] {0,0}
+        System.out.println(Arrays.toString(findMine( new int[][] { {1, 0, 0}, {0, 0, 0}, {0, 0, 0} } )));//new int[] {0,0}
+        System.out.println(Arrays.toString(findMine( new int[][] { {0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 1, 0}, {0, 0, 0, 0} } )));//new int[] {2,2}
+    }
+    public static int[] findMine(int[][] field){
+        for(int i=0; i<field.length;i++){
+            for (int j=0; j<field[i].length;j++){
+                if(field[i][j]==1) return new int[]{i,j};
+            }
+        }
+        return new int[] {};
+    }
+
+}
